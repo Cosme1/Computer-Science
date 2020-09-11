e988bfc383da06fd9f3617b89cf0306e46dfa8b9
Cosme1 <cosme.schuenemsnn@gmail.com>
2020-09-11 20:01:53 +0200

    Update with array and clearScreen

--- GeometricSequence.java
+++ GeometricSequence.java
@@ -1,8 +1,10 @@
-package ComputerScience;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GeometricSequence {
     public static void main(String[] args) {
+        clearScreen();
+        /*
         System.out.println("Enter the first term of your arithmetic sequence: ");
         Scanner sc = new Scanner(System.in);
         double u1 = sc.nextDouble();
@@ -14,7 +16,35 @@ public class GeometricSequence {
         System.out.println("Enter the thrid term of your arithmetic sequence: ");
         sc = new Scanner(System.in);
         double u3 = sc.nextDouble();
+        */
 
-        System.out.println("Sequence is " + u1 + ", " + u2 + ", " + u3);
+        
+        System.out.println("How many terms do you want inside your sequence?");
+        Scanner scanner = new Scanner(System.in);
+        int terms = scanner.nextInt();
+        int array[] = new int[terms];
+        System.out.println("Enter the first few terms of your arithmetic sequence: ");
+        
+        for (int i = 0; i < array.length; i++){
+            System.out.println("Please enter a number");
+            array[i] = scanner.nextInt();
+        }
+        
+        int difference = array[1] - array[0];
+        int nextTerm = array[2] + difference;
+        int U100 = array[0] + 99 * difference;
+        
+        clearScreen();
+        System.out.println("Results:");
+        System.out.println("Your Sequence is " + Arrays.toString(array));
+        System.out.println("The difference of the Sequence is " + difference);
+        System.out.println("The fourth term of the Sequence is " + nextTerm);
+        System.out.println("The formula of the Sequence is: Un = " + array[0] + "+(n-1)" + difference);
+        System.out.println("For example if n = 100, the formula is: U100 = " + array[0] + "+(100-1)" + difference);
+        System.out.println("In this case, U100 then equals " + U100);
+    }
+
+    public static void clearScreen() {
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));
     }
 }

19a60f32b7ea3fe88be76d6e0aebbd7c37e33da9
Cosme1 <cosme.schuenemsnn@gmail.com>
2020-09-19 18:45:45 +0200

    Fibonaci start

--- Fibonaci.java
+++ Fibonaci.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 public class Fibonaci {
     public static void main(String[] args) {
         
+        System.out.println("Type how many terms of the Fibonaci sequence you want");
+        Scanner scanner = new Scanner(System.in);
+        int terms = scanner.nextInt();
+        int array[] = new int[terms];
+        for (int i = 0; i <= terms; i++){
+            
+        }
     }
 }

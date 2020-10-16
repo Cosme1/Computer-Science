57b6c8eeab5d55f5ad7ce67686a28be73864347d
Cosme1 <cosme.schuenemsnn@gmail.com>
2020-10-16 18:48:46 +0200

    Fibonaci Sequence start

--- FactorialAndCombination.java
+++ FactorialAndCombination.java
@@ -5,7 +5,7 @@ import java.util.*;
  */
 public class FactorialAndCombination {
     public static void main(String[] args) {
-        System.out.println("Do you want to calculate Factorial or Combination");
+        System.out.println("Do you want to calculate Factorial or Combination or Binomial");
         Scanner scann = new Scanner(System.in);
         String input = scann.nextLine();
         if (input.equals("Factorial")){
@@ -20,7 +20,13 @@ public class FactorialAndCombination {
             int n = scann.nextInt();
             scann = new Scanner(System.in);
             int r = scann.nextInt();
-            Combination(n, r);
+            System.out.println(Combination(n, r));
+        }
+        else if (input.equals("Binomial")){
+            System.out.println("Type in n");
+            scann = new Scanner(System.in);
+            int n = scann.nextInt();
+            Binomial(n);
         }
         else System.out.println("Error, wrong input");
 
@@ -35,10 +41,17 @@ public class FactorialAndCombination {
             return (n * Factorial(n-1));
     }
 
-    static void Combination(int n, int r){
+    public static int Combination(int n, int r){
         int a = Factorial(n);
         int b = Factorial(r);
         int c = Factorial(n - r);
-        System.out.println(a/(b*c));
+        return (a/(b*c));
+    }
+
+    static void Binomial(int n){
+        
+        for(int i = 0; i < (n+1); i++){
+            System.out.print(Combination(n, i) + "a^" + (n-i) + "b^" + i + " + ");
+        }
     }
 }

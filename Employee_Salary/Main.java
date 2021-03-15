eadac0cd0f2602c9751c71fcc5db2e00eadc261d
Cosme1 <cosme.schuenemsnn@gmail.com>
2021-03-15 12:54:52 +0100

    worked on Main.java

--- Employee_Salary/Main.java
+++ Employee_Salary/Main.java
@@ -1,24 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		LinkedList Hemployee = new LinkedList<Hourly>();
+		LinkedList Semployee = new LinkedList<Salary>();
+		char again = 'n';
 		clearScreen();
-		System.out.println("Add Employee\n============= \n");
-		System.out.println("Enter Employee Payment Type\n1. Hourly\n2. Salary\n");
-		System.out.println("Make Selection -->");
-		String payment = input.nextLine();
-		int pay = Integer.parseInt(payment);
-		if (pay == 1) {
-			
-		}
-		else if (pay == 2) {
+		while (again == 'n'){
+			Scanner input = new Scanner(System.in);
+			System.out.println("Add Employee\n============= \n");
+			System.out.println("Enter Employee Payment Type\n1. Hourly\n2. Salary\n");
+			System.out.print("Make Selection -->");
+			String payment = input.nextLine();
+			int pay = Integer.parseInt(payment);
+			if (pay == 1) {
+				System.out.print("\nEnter Employee Name -->");
+				String name = input.nextLine();
+				System.out.println("\nEnter Employee SS# -->");
+				String ssNum = input.nextLine();
+				System.out.println("\nEnter hourly wage -->");
+				String hWage1 = input.nextLine();
+				double hWage = Integer.parseInt(hWage1);
+				System.out.println("\nEnter hours worked for month -->");
+				String hWorkedforMonth1 = input.nextLine();
+				double hWorkedforMonth = Integer.parseInt(hWorkedforMonth1);
+				Hourly employeeHourly = new Hourly(name, ssNum, hWage, hWorkedforMonth);
+				System.out.println("\nEnter another employee[y/n]?");
+				String again1 = input.nextLine();
+				again = again1.charAt(0);
+			}
+			else if (pay == 2) {
 
+			}
+			else{
+				System.out.println("Wrong input, please type in 1 or 2");
+			}
 		}
-		else{
 
-		}
-		
+		System.out.println("\n \n Salary Summary\n===============");
+		System.out.println("Name         = ");
 	}
 
 	public static void clearScreen() {  
